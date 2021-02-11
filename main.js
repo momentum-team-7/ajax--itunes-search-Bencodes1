b4e7975a729b0da533c747e66674a7009ce20f4a
Bencodes1 <256.ben@gmail.com>
2021-02-10 22:07:00 -0500

    updated js and html, need to get audio working

--- main.js
+++ main.js
@@ -2,26 +2,28 @@ const baseURL = 'https://itunes.apple.com/search?term='
 const rootElement = document.querySelector('.container')
 const form = document.querySelector('#search-form')
 const searchResults = document.querySelector('.search-results')
+const player = document.querySelector('sample-player')
 
 form.addEventListener('submit', e => { 
     e.preventDefault()
-    let searchInput = document.querySelector(".search-input").value;
-    search(searchInput)
+    clearResults()
+    search()
 })
 
-
 let searchInput = document.querySelector("#search-bar")
-search(searchInput)
 
 // Fetch data from itunes API with search input from user
-function search(searchInput){
-    let fetchURL = baseURL + searchInput + "&entity=song"
+function search(){
+    let searchInput = document.querySelector(".search-input").value;
+    let fetchURL = baseURL + searchInput + "&entity=song&limit=5"
     fetch(fetchURL) 
     .then(response => response.json())
     .then(data => {
         console.log(data.results[2].artistName)
         renderAll(data.results)
         })
+    .catch(error => 
+        catchError())    
 };
 
 // Takes full function returned from fetch request, returns array composed of
@@ -31,47 +33,57 @@ function renderAll(array){
 
 //      Rendering section: 
         const mainEl = document.createElement('div')
-        mainEl.className    = 'song-card'
-        mainEl.id           = object.trackName //not sure if necessary
+        mainEl.className        = 'song-card'
+        mainEl.id               = object.trackName //not sure if necessary
 
-        const artistEl      = document.createElement('h2')
-        artistEl.innerText  = object.artistName 
+        const artistEl          = document.createElement('h2')
+        artistEl.innerText      = object.artistName 
 
-        const trackEl       = document.createElement('h3')
-        trackEl.innerText   = object.trackName
+        const trackEl           = document.createElement('h3')
+        trackEl.innerText       = object.trackName
 
-        const albumEl       = document.createElement('h4')
-        albumEl.innerText   = object.collectionName
+        const albumEl           = document.createElement('h4')
+        albumEl.innerText       = object.collectionName
 
-        const albumImgEl    = document.createElement('img')
-        albumImgEl.src      = object.artworkUrl100
+        const albumImgEl        = document.createElement('img')
+        albumImgEl.src          = object.artworkUrl100
+
+        const playButtonEl      = document.createElement('button')
+        playButtonEl.innerText  = "Play Sample"
 
         mainEl.appendChild(albumImgEl)
         mainEl.appendChild(trackEl)
         mainEl.appendChild(artistEl)
         mainEl.appendChild(albumEl)
+        mainEl.appendChild(playButtonEl)
 
         searchResults.appendChild(mainEl)
-    }
+
+        playButtonEl.addEventListener('click', (event) => {
+            playSample(event.target)
+        })
+    };
 }
 
+function clearResults() {
+    let mainElements = document.querySelectorAll('.song-card')
+    for (let item of mainElements) {
+        item.remove();
+    };
+}
 
+function catchError(){
+    const errorEl = document.createElement('div')
+    errorEl.innerText = "No results found for this query"
+    searchResults.appendChild(errorEl)
+}
 
-// musicEl.classname = 'sample string'
+function playSample(button){
+    let audioSample = document.querySelector('audio')
+    console.log(audioSample)
+    audioSample.src = button.parentElement.dataset.previewUrl
+    console.log(button.parentElement.dataset.previewUrl)
+}
 
-// let testdivcreator = document.createElement("div")
-
-
-
-        // elements (delete when done)
-        // console.log(
-        //     object.artistName,              "\n", 
-        //     object.trackName,               "\n", 
-        //     object.isStreamable,            "\n", 
-        //     object.kind,                    "\n", 
-        //     object.collectionName,          "\n", 
-        //     object.collectionCensoredName,  "\n", 
-        //     object.artworkUrl100,           "\n", 
-        //     object.artistViewUrl,           "\n", 
-        //     object.previewUrl,              "\n", 
-        //     object.collectionViewUrl            );
+// default image (black square)
+// src="https://v.dreamwidth.org/8585989/234887" 
